c8ab38348d8ac35e63d01ea88f52224a8147cbae
Elliot Sayes <elliot@sayes.cloud>
2023-11-07 18:58:14 +0700

    Published branch "dev"

---
+++ dist/index.d.ts
@@ -0,0 +1,24 @@
+declare namespace _default {
+    /**
+     * @param {Env} env
+     * @returns {Payments}
+     */
+    function init(env: Env): Payments;
+}
+export default _default;
+export type Env = {
+    warp: any;
+    wallet: any;
+    /**
+     * - url for arweave graphql server
+     */
+    gateway: string;
+};
+export type Licensed = (contract: string, addr: string) => Promise<boolean>;
+export type Pay = (contract: string, address: string) => Promise<{
+    ok: boolean;
+}>;
+export type Payments = {
+    isLicensed: Licensed;
+    pay: Pay;
+};
